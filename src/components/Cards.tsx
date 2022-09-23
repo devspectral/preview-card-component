d2d72b4506c7a2be2c5ca3ff2ee86a0d8b276a04
unknown <devspectral@gmail.com>
2022-09-23 13:55:06 -0400

    ToDo: Change font and finalize

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -7,41 +7,41 @@ import icon3 from '../assets/images/icon-luxury.svg'
 
 const Cards = () => {
     return (<>
-        <div className='justify-center items-center p-4'>
-            <div className='my-8 flex lg:flex-row flex-col'>
-                <div className='h-[32rem] w-[28rem] bg-[#e38826]'>
+        <div className='justify-center items-center p-4 text-white'>
+            <div className='w-full m-2 flex lg:flex-row flex-col'>
+                <div className='h-[32rem] w-[20rem] bg-[#e38826] p-10'>
                     {/* icon */}
-                    <div><img src={icon1} alt="Logo" /></div>
+                    <div className='my-4'><img src={icon1} alt="Logo" /></div>
                     {/* title */}
-                    <div>{data.Card1.title}</div>
+                    <div className='my-10'>{data.Card1.title}</div>
                     {/* subtitle */}
-                    <div>{data.Card1.subtitle}</div>
+                    <div className='my-10'>{data.Card1.subtitle}</div>
                     {/* Button */}
-                    <div>
+                    <div className='mt-32'>
                         <Buttons btnColor={"text-[#e38826]"} />
                     </div>
                 </div>
-                <div className='h-[32rem] w-[28rem] bg-[#006970]'>
+                <div className='h-[32rem] w-[20rem] bg-[#006970] p-10'>
                     {/* icon */}
-                    <div><img src={icon2} alt="Logo" /></div>
+                    <div className='my-4'><img src={icon2} alt="Logo" /></div>
                     {/* title */}
-                    <div>{data.Card2.title}</div>
+                    <div className='my-10'>{data.Card2.title}</div>
                     {/* subtitle */}
-                    <div>{data.Card2.subtitle}</div>
+                    <div className='my-10'>{data.Card2.subtitle}</div>
                     {/* Button */}
-                    <div>
+                    <div className='mt-32'>
                         <Buttons btnColor={"text-[#006970]"} />
                     </div>
                 </div>
-                <div className='h-[32rem] w-[28rem] bg-[#004241]'>
+                <div className='h-[32rem] w-[20rem] bg-[#004241] p-10'>
                     {/* icon */}
-                    <div><img src={icon3} alt="Logo" /></div>
+                    <div className='my-4'><img src={icon3} alt="Logo" /></div>
                     {/* title */}
-                    <div>{data.Card3.title}</div>
+                    <div className="my-10">{data.Card3.title}</div>
                     {/* subtitle */}
-                    <div>{data.Card3.subtitle}</div>
+                    <div className='my-10'>{data.Card3.subtitle}</div>
                     {/* Button */}
-                    <div>
+                    <div className='mt-32'>
                         <Buttons btnColor={"text-[#004241]"} />
                     </div>
                 </div>
--- src/components/MainComponent.tsx
+++ src/components/MainComponent.tsx
@@ -6,11 +6,11 @@ import Cards from '../components/Cards'
 const MainComponent = () => {
     return (
         <>
-            <div className='bg-white text-black'>
+            
                 <div className='h-screen flex flex-wrap items-center justify-center' >
                     <Cards />
                 </div>
-            </div>
+            
         </>
     )
 }
